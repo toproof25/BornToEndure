b9f2a5ba14c8f05d6aaace7b4446742d43f37d76
toproof <kmnlmn123gmail.com>
2026-04-02 00:31:38 +0900

    feat: Pet의 공격 시스템 컴포넌트 구현
    - 공격 함수 위임하는 함수 추가
    - 공격 역할을 담당하는 컴포넌트 추가

--- Source/BornToEndure/Character/Pet/PetCompanionCharacter.h
+++ Source/BornToEndure/Character/Pet/PetCompanionCharacter.h
@@ -5,6 +5,7 @@
 #include "PetCompanionCharacter.generated.h"
 
 class USphereComponent;
+class UPetCombatComponent;
 
 UCLASS()
 class BORNTOENDURE_API APetCompanionCharacter : public ACharacter
@@ -19,15 +20,25 @@ protected:
 
 public:	
 	virtual void Tick(float DeltaTime) override;
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
 
 
 public:
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+	TObjectPtr<UPetCombatComponent> PetCombatComp;
+
 	/**
 	 * @brief 적 탐지 충돌체 컴포넌트
+	 * @note 현재는 사용할 수 있으나 추후에는 다른 방식으로 변경할 예정
 	 */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	TObjectPtr<USphereComponent> DetectionSphere;
+
+	/**
+	 * @brief PetCombatComp의 공격 함수를 위임한 함수
+	 * @param TargetVector 공격할 적 위치
+	 * @note UBTTask_PetAttack에서 호출됩니다
+	 */
+	void OnAttack(const FVector& TargetVector);
+
 };
---
+++ Source/BornToEndure/Component/PetCombatComponent.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Delegates/Delegate.h"
+#include "PetCombatComponent.generated.h"
+
+
+class UArrowComponent;
+class ABaseProjectile;
+
+
+// Delegate 선언
+DECLARE_DELEGATE_TwoParams(FSpawnSoundAtLocation, FName, FVector);
+DECLARE_DELEGATE_TwoParams(FSpawnNiagaraAtLocation, FName, FVector);
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BORNTOENDURE_API UPetCombatComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	UPetCombatComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+
+	/** * @brief Delegate 시그니처 선언으로, 공격 시 사운드와 나이아가라 스폰 호출 */
+	FSpawnSoundAtLocation SoundDelegate;
+	FSpawnNiagaraAtLocation NiagaraDelegate;
+
+	//UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	//TObjectPtr<UArrowComponent> ProjectilePoint;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Projectile")
+	TSubclassOf<ABaseProjectile> ProjectileClass;
+
+	/** * @brief UProjectilePoolSubsystem에서 사용할 풀 크기를 설정하는 변수 */
+	UPROPERTY(EditAnywhere, Category = "Projectile")
+	int32 ProjectilePoolSize;
+
+	/** * @brief 사운드와 나이아가라 애셋을 참조하는 변수 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Projectile|Effect|Sound", meta = (AllowedTypes = "SoundDataAsset"))
+	FPrimaryAssetId AttackSoundId;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Projectile|Effect|Niagara", meta = (AllowedTypes = "NiagaraDataAsset"))
+	FPrimaryAssetId AttackNiagaraId;
+
+
+	UFUNCTION()
+	void OnAttack(const FVector& TargetVector);
+
+
+private:
+
+	void OnAttackSound(const FVector& SpawnLocation) const;
+	void OnAttackNiagara(const FVector& SpawnLocation) const;
+
+	/** * @brief발사체 풀을 초기화하는 함수 */
+	void InitializeProjectilePool();
+
+};
