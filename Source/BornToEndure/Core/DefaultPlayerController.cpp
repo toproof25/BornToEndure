352c429312a991eb4f61755e4353b4985d0fc784
toproof <kmnlmn123gmail.com>
2026-07-10 22:34:02 +0900

    feat: Lobby Level Soft 참조 변수 추가 및 이동 함수 구현
    - TSoftObjectPtr<UWorld> 타입의 LobbyLevel을 참조하는 멤버 변수 추가. 에디터에서 지정하여 사용
    - LobbyLevel로 이동하는 `TravelToLobbyLevel` 함수 추가 -> GameOverWidget의 Exit버튼을 누를 때 호출이 됨

--- Source/BornToEndure/Core/DefaultPlayerController.cpp
+++ Source/BornToEndure/Core/DefaultPlayerController.cpp
@@ -1,6 +1,7 @@
 ﻿#include "Core/DefaultPlayerController.h"
 
 #include "EnhancedInputSubsystems.h"
+#include "Kismet/GameplayStatics.h"
 
 #include "UI/LevelUpRewardWidget.h"
 #include "UI/PlayerHUDWidget.h"
@@ -83,6 +84,23 @@ void ADefaultPlayerController::RestoreGameInputMode()
 	SetGameInputMode();
 }
 
+void ADefaultPlayerController::TravelToLobbyLevel()
+{
+	if (LobbyLevel.IsNull())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("LobbyLevel is not set. Please set a valid level in the DefaultPlayerController."));
+		return;
+	}
+
+	UGameplayStatics::OpenLevelBySoftObjectPtr(
+		this,
+		LobbyLevel,
+		true,
+		FString()
+	);
+	UE_LOG(LogTemp, Log, TEXT("Traveling to Lobby Level: %s"), *LobbyLevel.ToString());
+}
+
 void ADefaultPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
@@ -90,6 +108,7 @@ void ADefaultPlayerController::BeginPlay()
 	SetUpPlayerInputMode();
 	SetUpPlayerHUDWidget();
 	SetUpDelegates();
+	SetGameInputMode();
 }
 
 void ADefaultPlayerController::OnPossess(APawn* InPawn)
